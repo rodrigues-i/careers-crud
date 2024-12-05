0c3694fcb0008bb48cf37324a85b70fee079a888
rodrigues-i <rodrigues-i@outlook.com>
2024-12-05 19:40:06 -0300

    Create automatically the created_datetime field

--- careers/models.py
+++ careers/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Career(models.Model):
     username = models.CharField(max_length=100)
-    created_datetime = models.DateTimeField()
+    created_datetime = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100)
     content = models.CharField(max_length=100)
 
