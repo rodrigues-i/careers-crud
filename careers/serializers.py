2b55c3803485fd02ed3c15448fe7b6c6939509cb
rodrigues-i <rodrigues-i@outlook.com>
2024-12-05 22:24:32 -0300

    Remove comments

--- careers/serializers.py
+++ careers/serializers.py
@@ -6,20 +6,20 @@ class CareerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Career
         fields = ['id', 'username', 'created_datetime', 'title', 'content']
-        read_only_fields = ['id', 'created_datetime']  # Explicitly set read-only fields
+        read_only_fields = ['id', 'created_datetime']
 
     def update(self, instance, validated_data):
-        # Allowed fields for update
+
         allowed_fields = ['title', 'content']
 
-        # Compare the fields in the request data (not validated_data) to find invalid fields
+
         invalid_fields = [field for field in self.initial_data.keys() if field not in allowed_fields]
 
-        # Check for invalid fields and raise validation error
+
         if invalid_fields:
             raise serializers.ValidationError({
                 'invalid_fields': f"These fields cannot be updated: {', '.join(invalid_fields)}"
             })
 
-        # Proceed with the update
+
         return super().update(instance, validated_data)
